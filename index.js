9aae6648bfd2728921a5075e8b2af926de96adb3
Seth Bergman <seth.atxwebs@gmail.com>
2018-04-30 23:45:50 -0500

    localized assets

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use('/assets', express.static(__dirname + '/assets'))
 
 app.get('/', function(request, response) {
   response.render('pages/index');
