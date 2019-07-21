e8813f63f3118eece988daf63b0634983feb3e83
Seth Bergman <seth_bergman@dellteam.com>
2019-07-21 07:08:48 -0500

    Updated server.js to ES6 syntax

--- server.js
+++ server.js
@@ -7,50 +7,50 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
-app.get('/', function(request, response) {
-  response.render('pages/index')
+app.get('/', (req, res) => {
+  res.render('pages/index')
 })
-app.get('/codebase', function(request, response) {
-  response.render('pages/codebase')
+app.get('/codebase', (req, res) => {
+  res.render('pages/codebase')
 })
-app.get('/dependencies', function(request, response) {
-  response.render('pages/dependencies')
+app.get('/dependencies', (req, res) => {
+  res.render('pages/dependencies')
 })
-app.get('/config', function(request, response) {
-  response.render('pages/config')
+app.get('/config', (req, res) => {
+  res.render('pages/config')
 })
-app.get('/backing-services', function(request, response) {
-  response.render('pages/backing-services')
+app.get('/backing-services', (req, res) => {
+  res.render('pages/backing-services')
 })
-app.get('/build-release-run', function(request, response) {
-  response.render('pages/build-release-run')
+app.get('/build-release-run', (req, res) => {
+  res.render('pages/build-release-run')
 })
-app.get('/processes', function(request, response) {
-  response.render('pages/processes')
+app.get('/processes', (req, res) => {
+  res.render('pages/processes')
 })
-app.get('/port-binding', function(request, response) {
-  response.render('pages/port-binding')
+app.get('/port-binding', (req, res) => {
+  res.render('pages/port-binding')
 })
-app.get('/concurrency', function(request, response) {
-  response.render('pages/concurrency')
+app.get('/concurrency', (req, res) => {
+  res.render('pages/concurrency')
 })
-app.get('/disposability', function(request, response) {
-  response.render('pages/disposability')
+app.get('/disposability', (req, res) => {
+  res.render('pages/disposability')
 })
-app.get('/dev-prod-parity', function(request, response) {
-  response.render('pages/dev-prod-parity')
+app.get('/dev-prod-parity', (req, res) => {
+  res.render('pages/dev-prod-parity')
 })
-app.get('/logs', function(request, response) {
-  response.render('pages/logs')
+app.get('/logs', (req, res) => {
+  res.render('pages/logs')
 })
-app.get('/admin-processes', function(request, response) {
-  response.render('pages/admin-processes')
+app.get('/admin-processes', (req, res) => {
+  res.render('pages/admin-processes')
 })
 
 app.use(errorHandler())
 
-const server = app.listen(process.env.PORT || 5000, function() {
-  console.log('Server running at http://0.0.0.0:' + server.address().port)
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Server running at http://localhost:' + server.address().port)
 })
 
 module.exports = server
